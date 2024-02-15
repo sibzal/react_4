dcbba422fbcc223c77436f72ed88e21201a9d59e
Сибгатуллина Залия Анисовна <242017@mck-ktits.ru>
2024-02-15 10:40:28 +0300

    commit

---
+++ src/components/Catalog/Catalog.jsx
@@ -0,0 +1,14 @@
+import Card from "../Card/Card"
+
+export default function Catalog (){
+    return (
+        <div className="catalog-content">
+            <Card name="Рубашка СВЭГ" price="1000"/>
+            <Card name="Рубашка классная" price="2000"/>
+            <Card name="Штаны классика" price="3500"/>
+            <Card name="Рубашка СВЭГ" price="1000"/>
+            <Card name="Юбка карандаш" price="500"/>
+            <Card name="Рубашка СВЭГ" price="1320"/>
+        </div>
+    )
+}
